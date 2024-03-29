8f8f003b539e771dd1ed848d45ddaa5f4868ef10
tuyishimejeandamour <tuyishimejeand88@gmail.com>
2024-03-29 20:54:25 +0200

    fix: add to meter

--- meter.py
+++ meter.py
@@ -2,18 +2,13 @@ class Meter:
     def __init__(self, meter_id, nickname=None):
         self.meter_id = meter_id
         self.nickname = nickname
-        self.balance = 0
-        self.top_up_history = []
+        self.tokens = {}  
 
     def update_nickname(self, new_nickname):
         self.nickname = new_nickname
 
-    def add_credit(self, amount):
-        self.balance += amount
-        self.top_up_history.append((amount, "Credit added"))
+    def add_token(self, token, amount):
+        self.tokens[token] = amount
 
-    def get_balance(self):
-        return self.balance
-
-    def get_top_up_history(self):
-        return self.top_up_history
+    def get_tokens(self):
+        return self.tokens
