957a850c51b4f0a124d6afa60a4c77a8d97aa267
Kwizera Emmanuel <51673007+Emmanuel-kwizera@users.noreply.github.com>
2024-03-29 19:08:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+def admin_dash_menu():
+    print("\t       Welcome to the EVA panel. Here is the menu\n")
+    print("\t_______________________________________________________\n")
+    print("\t     1. Buy Electricity \n")
+    print("\t     2. View Recent Receipts \n")
+    print("\t     3. Exit\n")
+    print("\n\n\t_______________________________________________________\n")
+    choice = int(input("\t\t\t Enter your choice      : "))
+    return choice
+
+def main():
+    choice = admin_dash_menu()
+    print("You entered:", choice)
+
+if __name__ == "__main__":
+    main()
 
