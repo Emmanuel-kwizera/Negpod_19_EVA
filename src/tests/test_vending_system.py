587d4de24d5b1f6e52f0c4b713a11f7bd946d8d3
jacques-twizeyimana <sandbergjacques500@gmail.com>
2024-04-18 09:32:59 +0200

    comment unneeded codes

--- src/tests/test_vending_system.py
+++ src/tests/test_vending_system.py
@@ -1,28 +1,39 @@
+# import unittest
+#
+#
+# import unittest
+# from unittest.mock import patch
+# from src.app.vending_system import ElectricityVendingSystem
+# from src.app.meter import Meter
+# from src.database.manager import DatabaseManager
+#
+# class TestElectricityVendingSystem(unittest.TestCase):
+#     @patch('src.database.manager.DatabaseManager.get_meter')
+#     @patch('src.database.manager.DatabaseManager.save_meter')
+#     @patch('src.database.manager.DatabaseManager.save_token')
+#     def test_purchase_token(self, mock_save_token, mock_save_meter, mock_get_meter):
+#         mock_get_meter.return_value = Meter("123", "Test Meter", {})
+#         system = ElectricityVendingSystem()
+#         system.purchase_token("123", 20.0)
+#         mock_get_meter.assert_called_once_with("123")
+#         mock_save_meter.assert_called_once_with(mock_get_meter.return_value)
+#         mock_save_token.assert_called_once_with("token", 20.0, "123")
+#         self.assertEqual(mock_get_meter.return_value.tokens, {"token": 20.0})
+#
+#     @patch('src.database.manager.DatabaseManager.get_meter')
+#     def test_view_tokens(self, mock_get_meter):
+#         mock_get_meter.return_value = Meter("123", "Test Meter", {"token1": 10.0, "token2": 20.0})
+#         system = ElectricityVendingSystem()
+#         system.view_tokens("123")
+#         mock_get_meter.assert_called_once_with("123")
+
 import unittest
 
 
-import unittest
-from unittest.mock import patch
-from src.app.vending_system import ElectricityVendingSystem
-from src.app.meter import Meter
-from src.database.manager import DatabaseManager
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
 
-class TestElectricityVendingSystem(unittest.TestCase):
-    @patch('src.database.manager.DatabaseManager.get_meter')
-    @patch('src.database.manager.DatabaseManager.save_meter')
-    @patch('src.database.manager.DatabaseManager.save_token')
-    def test_purchase_token(self, mock_save_token, mock_save_meter, mock_get_meter):
-        mock_get_meter.return_value = Meter("123", "Test Meter", {})
-        system = ElectricityVendingSystem()
-        system.purchase_token("123", 20.0)
-        mock_get_meter.assert_called_once_with("123")
-        mock_save_meter.assert_called_once_with(mock_get_meter.return_value)
-        mock_save_token.assert_called_once_with("token", 20.0, "123")
-        self.assertEqual(mock_get_meter.return_value.tokens, {"token": 20.0})
 
-    @patch('src.database.manager.DatabaseManager.get_meter')
-    def test_view_tokens(self, mock_get_meter):
-        mock_get_meter.return_value = Meter("123", "Test Meter", {"token1": 10.0, "token2": 20.0})
-        system = ElectricityVendingSystem()
-        system.view_tokens("123")
-        mock_get_meter.assert_called_once_with("123")
+if __name__ == '__main__':
+    unittest.main()
