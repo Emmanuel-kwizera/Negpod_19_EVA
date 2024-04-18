45e8853772c7487e6c4d0ef91d54ff26c829730f
jacques-twizeyimana <sandbergjacques500@gmail.com>
2024-04-18 09:33:32 +0200

    feat: electricity vending system main menu

--- src/app/vending_system.py
+++ src/app/vending_system.py
@@ -1,33 +1,39 @@
-import secrets
-import string
-
-from src.database.connection import DatabaseManager
+from src.app.menu import Menu
 
 
 class ElectricityVendingSystem:
     def __init__(self):
-        self.db_manager = DatabaseManager()
+        pass
 
-    def generate_token(self, token_length=10):
-        characters = string.digits
-        return ''.join(secrets.choice(characters) for _ in range(token_length))
+    @staticmethod
+    def main_menu():
+        main_menu = Menu("Electricity Vending System", [
+            "1. Meter Management",
+            "2. Purchase Electricity",
+            "3. Load Token",
+            "4. View Account Information",
+            "5. Help",
+            "6. Exit"
+        ])
+        main_menu.display()
+        choice = main_menu.get_user_choice()
+        return choice
 
-    def purchase_token(self, meter_id, amount):
-        token = self.generate_token()
-        meter = self.get_meter(meter_id)
-        meter.add_token(token, amount)
-        self.db_manager.save_meter(meter)
-        self.db_manager.save_token(token, amount, meter_id)
-        print(f"Token {token} worth ${amount} has been generated for meter {meter_id}.")
+    @staticmethod
+    def help_submenu():
+        """
+        Prints a help menu with explanations for functionalities.
+        """
+        print("\nHelp Menu:")
+        print("1. Meter Management:")
+        print("\t- Create Meter: Add a new meter to the system and associate it with a location.")
+        print("\t- View All Meters: List all meters and their details (meter number, location).")
+        print("\t- Find Meter by Number: Search for a specific meter and view its details.")
+        print("\t- Add Token: Associate a token with a meter for future electricity purchases.")
+        print("\t- Remove Token: Disassociate a token from a meter if it's no longer needed.")
+        print("2. Purchase Electricity: (Not implemented yet)")
+        print(
+            "\t- This functionality will allow users to purchase electricity using tokens associated with their meters.")
+        print("3. View Account Information: (Not implemented yet)")
+        print("\t- This functionality will display account-related information (purchase history, balance, etc.).")
 
-    def get_meter(self, meter_id):
-        return self.db_manager.get_meter(meter_id)
-
-    def view_tokens(self, meter_id):
-        meter = self.get_meter(meter_id)
-        tokens = meter.get_tokens()
-        for token, amount in tokens.items():
-            print(f"- Token: {token}, Amount: ${amount}")
-
-    def close(self):
-        self.db_manager.close()
